2a832e868c99b9075f8cb6472feac46acd2df481
Pedro Karsan <pkarsan@qaworks.com>
2016-08-18 11:25:04 +0100

    Console application with basic unit test

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        private List<float> grades = new List<float>();
+
+        public GradeBook()
+        {
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+
+            foreach (var grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+    }
+}
---
+++ Grades/GradeStatistics.cs
@@ -0,0 +1,15 @@
+﻿namespace Grades
+{
+    public class GradeStatistics
+    {
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
+
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+    }
+}
