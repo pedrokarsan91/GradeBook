92491a748690a9c5eca9477517625e1e24790240
Pedro Karsan <pkarsan@qaworks.com>
2016-08-23 11:02:39 +0100

    Added branching (if, else if loops), switching (switch case)

--- Grades.Tests/GradeBookTests.cs
+++ Grades.Tests/GradeBookTests.cs
@@ -41,5 +41,70 @@ namespace Grades.Tests
 
             Assert.AreEqual(85.16, result.AverageGrade, 0.01);
         }
+
+        [TestMethod]
+        public void LetterGradeATest()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(93.5f);
+            book.AddGrade(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+
+            Assert.AreEqual("A", result.LetterGrade);
+        }
+
+        [TestMethod]
+        public void LetterGradeBTest()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(81);
+            book.AddGrade(86.2f);
+            book.AddGrade(88.4f);
+
+            GradeStatistics result = book.ComputeStatistics();
+
+            Assert.AreEqual("B", result.LetterGrade);
+        }
+
+        [TestMethod]
+        public void LetterGradeCTest()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(70);
+            book.AddGrade(73.6f);
+            book.AddGrade(77.4f);
+
+            GradeStatistics result = book.ComputeStatistics();
+
+            Assert.AreEqual("C", result.LetterGrade);
+        }
+
+        [TestMethod]
+        public void LetterGradeDTest()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(62.5f);
+            book.AddGrade(66);
+            book.AddGrade(68.2f);
+
+            GradeStatistics result = book.ComputeStatistics();
+
+            Assert.AreEqual("D", result.LetterGrade);
+        }
+
+        [TestMethod]
+        public void LetterGradeFTest()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(52.3f);
+            book.AddGrade(55);
+            book.AddGrade(58.5f);
+
+            GradeStatistics result = book.ComputeStatistics();
+
+            Assert.AreEqual("F", result.LetterGrade);
+        }
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -7,35 +7,28 @@ namespace Grades
         static void Main(string[] args)
         {
             GradeBook book = new GradeBook();
-            book.NameChanged += OnNameChanged;
-
-            book.Name = "Pedro's book";
-
+           
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
 
-            Console.WriteLine(book.Name);
             WriteResult("Average", stats.AverageGrade);
-            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Highest", stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
+
+            WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine("{0}: {1}", description, result);
         }
 
         static void WriteResult(string description, float result)
         {
             Console.WriteLine("{0}: {1:F2}", description, result);
-        }
-
-        static void WriteResult(string description, int result)
-        {
-            Console.WriteLine(description + ": " + result);
-        }
-
-        static void OnNameChanged(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine($"Gradebook changing name from {args.ExistingName} to {args.NewName}");
         }
     }
 }
